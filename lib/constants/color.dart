4be4e01a7947d627e9be3d0669ccdc9628bbdbd7
Mac_Developer <araozangel842@gmail.com>
2023-01-05 17:13:37 +0200

    Starting app with some structure

---
+++ lib/constants/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFE53935);
+const Color tdBlue = Color(0xFF1E88E5);
+
+const Color tdBlack = Color(0xFF000000);
+const Color tdWhite = Color(0xFFFFFFFF);
+const Color tdGrey = Color(0xFF9E9E9E);
+
+const Color tdBg = Color(0xFFF5F5F5);
